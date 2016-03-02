9829fda193e03c5b9cd274fed3a627df024a491a
saching123 <schngmg@gmail.com>
2016-03-02 16:13:22 +0530

    Adding files

---
+++ CalculatorJS.js
@@ -0,0 +1,14 @@
+<script>
+var xmlHttp=createXmlHttpRequest();
+    function createXmlHttpRequest(){
+    if(window.ActiveXObject){
+    try{
+    xmlHttp=new createXmlHttpRequest();
+    }
+    catch (e){
+    xmlHttp=false;
+    }
+    }else{
+    xmlHttp=new XML
+    }}
+</script>
